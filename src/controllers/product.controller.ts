de0b7a23aadc7604d1f302df136e311b7bae862c
Sarath-33 <withsarath.m@gmail.com>
2025-10-30 16:31:34 +0530

    completed product Controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -28,12 +28,44 @@ export const getProduct = async (req: Request, res: Response) => {
 };
 
 export const createProduct = async (req: Request, res: Response) => {
-    const {name, price, description, image} = req.body;
+  const { name, price, description, image } = req.body;
+  try {
+    if (!name || !price || !description || !image) {
+      return res.status(404).json({ message: "Every field is required" });
+    }
+    const product = new Product({ name, price, description, image });
+    const savedProduct = await product.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    console.log("You got a error! this is the error: ", error);
+  }
   res.send("Create Product");
 };
 export const updateProduct = async (req: Request, res: Response) => {
+  const { name, price, description, image } = req.body;
+  try {
+    if (!name || !price || !description || !image) {
+      return res.status(404).json({ message: "Every field is required" });
+    }
+    const updateProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      { name, price, description, image },
+      { new: true }
+    );
+  } catch (error) {
+    console.log("You got a error! this is the error: ", error);
+  }
   res.send("Update the product");
 };
 export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deleteProduct) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(201).json(deleteProduct);
+  } catch (error) {
+    console.log("You got a error! this is the error: ", error);
+  }
   res.send("Delete the product");
 };
