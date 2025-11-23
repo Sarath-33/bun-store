7944808ca3819bcd6f322f7f9ad5b047f16ae647
Sarath-33 <withsarath.m@gmail.com>
2025-11-23 19:30:14 +0530

    added code in errorhandler

--- src/middleware/errorHander.ts
+++ src/middleware/errorHander.ts
@@ -1,6 +1,8 @@
 import type { Request, Response } from "express";
-export const errorHandler = (err:Error, req: Request, res:Response, next: any) => {
+const errorHandler = (err:Error, req: Request, res:Response, next: any) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Internal Server Error' });
   next()
 };
+
+export default errorHandler;
